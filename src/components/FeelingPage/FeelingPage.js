a1f014be359f226db9ac318b94b1fdff0eb6e1e3
Andrew Nguyen <anguyen1320@gmail.com>
2020-04-17 16:34:19 -0500

    added structure and style to feeling component

--- src/components/FeelingPage/FeelingPage.js
+++ src/components/FeelingPage/FeelingPage.js
@@ -4,7 +4,23 @@ class FeelingPage extends Component {
   render() {
     return (
       <div>
-        <h2>Feeling?</h2>
+        <h2>How are you feeling today?</h2>
+        <br />
+
+        <div>
+          <h4>Feeling?</h4>
+          <input
+            className="app-input"
+            type="number"
+            min="1"
+            max="5"
+            placeholder="1-5"
+          ></input>
+        </div>
+
+        <div>
+          <button className="app-btn">NEXT</button>
+        </div>
       </div>
     );
   }
