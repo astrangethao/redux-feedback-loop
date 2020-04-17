808ef8ed3455b8a4efbb7fb3027e58d81b61b1d6
Andrew Nguyen <anguyen1320@gmail.com>
2020-04-17 17:18:12 -0500

    setup the comment reducer, and the comment page component; it works

--- src/components/CommentPage/CommentPage.js
+++ src/components/CommentPage/CommentPage.js
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class CommentPage extends Component {
+  state = {
+    comment: "",
+  };
+
+  handleInputChange = (event) => {
+    this.setState({
+      comment: event.target.value,
+    });
+  };
+
+  handleNextClick = (event) => {
+    this.props.dispatch({
+      type: "SET_COMMENT",
+      payload: this.state.comment,
+    });
+    this.props.history.push("/review");
+  };
+
   render() {
     return (
       <div>
-        <h2>Comments?</h2>
+        <h2>Any comment you want to leave?</h2>
+        <br />
+
+        <div>
+          <h4>Comments</h4>
+          <input
+            onChange={this.handleInputChange}
+            className="app-input"
+            type="text"
+            placeholder="comment"
+          ></input>
+        </div>
+
+        <div>
+          <button onClick={this.handleNextClick} className="app-btn">
+            NEXT
+          </button>
+        </div>
       </div>
     );
   }
 }
 
-export default CommentPage;
+export default connect()(CommentPage);
--- src/index.js
+++ src/index.js
@@ -29,8 +29,20 @@ const supportReducer = (state = "", action) => {
   return state;
 };
 
+const commentReducer = (state = "", action) => {
+  if (action.type === "SET_COMMENT") {
+    return action.payload;
+  }
+  return state;
+};
+
 const storeInstance = createStore(
-  combineReducers({ feelingReducer, understandingReducer, supportReducer }),
+  combineReducers({
+    feelingReducer,
+    understandingReducer,
+    supportReducer,
+    commentReducer,
+  }),
   applyMiddleware(logger)
 );
 
