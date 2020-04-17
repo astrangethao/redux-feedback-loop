317cb021cd23e0578036e07407fe699e9e862dc2
Andrew Nguyen <anguyen1320@gmail.com>
2020-04-17 18:13:39 -0500

    completed basemode

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -4,6 +4,25 @@ const pool = require("../modules/pool");
 
 router.post("/", (req, res) => {
   console.log(req.body);
+
+  const queryText = `INSERT INTO "feedback"
+  (feeling, understanding, support, comments)
+  VALUES ($1, $2, $3, $4);`;
+
+  pool
+    .query(queryText, [
+      req.body.feeling,
+      req.body.understanding,
+      req.body.support,
+      req.body.comments,
+    ])
+    .then((response) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
