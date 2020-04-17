8c9e731f2784906c2cb5e59fe41283044b8d9935
Andrew Nguyen <anguyen1320@gmail.com>
2020-04-17 17:30:15 -0500

    set up the review page component, it works

--- src/components/ReviewPage/ReviewPage.js
+++ src/components/ReviewPage/ReviewPage.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class ReviewPage extends Component {
   render() {
     return (
       <div>
         <h2>Review Your Feedback</h2>
+        <div>
+          <h4>Feelings: {this.props.store.feelingReducer}</h4>
+          <h4>Understanding: {this.props.store.understandingReducer}</h4>
+          <h4>Support: {this.props.store.supportReducer}</h4>
+          <h4>Comments: {this.props.store.commentReducer}</h4>
+        </div>
+        <div>
+          <button className="app-btn">SUBMIT</button>
+        </div>
       </div>
     );
   }
 }
 
-export default ReviewPage;
+const mapStoreToProps = (store) => ({ store });
+export default connect(mapStoreToProps)(ReviewPage);
